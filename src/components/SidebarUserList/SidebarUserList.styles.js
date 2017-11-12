68589fdd7a4ae0a39f48cf42218ed90d535b5d7f
Marcin Madeja <madeja.m.b@gmail.com>
2017-11-12 09:58:15 +0100

    Add style for sidebar

---
+++ src/components/SidebarUserList/SidebarUserList.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h3`
+	padding: 10px 15px 0;
+	margin: 0 0 10px;
+	font-weight: 500;
+	font-size: 25px;
+`;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'styles/main.css';
+
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
