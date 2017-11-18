dfeccf279581e44880c23bdd69742c21c1bdd72c
Marcin Madeja <madeja.m.b@gmail.com>
2017-11-18 21:48:24 +0100

    Add react routers

---
+++ src/scenes/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquam numquam odio repellendus libero sit? Excepturi esse velit eligendi nobis aliquid perspiciatis, dolorem recusandae dicta voluptatibus animi. Possimus, in suscipit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquam numquam odio repellendus libero sit? Excepturi esse velit eligendi nobis aliquid perspiciatis, dolorem recusandae dicta voluptatibus animi. Possimus, in suscipit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquam numquam odio repellendus libero sit? Excepturi esse velit eligendi nobis aliquid perspiciatis, dolorem recusandae dicta voluptatibus animi. Possimus, in suscipit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquam numquam odio repellendus libero sit? Excepturi esse velit eligendi nobis aliquid perspiciatis, dolorem recusandae dicta voluptatibus animi. Possimus, in suscipit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquam numquam odio repellendus libero sit? Excepturi esse velit eligendi nobis aliquid perspiciatis, dolorem recusandae dicta voluptatibus animi. Possimus, in suscipit.</p>
+    </div>
+  );
+};
+
+export default About;
