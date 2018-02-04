40b74557b3077163450ed6ddbfdc0aadd8539da1
Marcin Madeja <madeja.m.b@gmail.com>
2018-02-04 22:39:50 +0100

    Apply middleware thunk, logger

--- src/app-redux/store/store.js
+++ src/app-redux/store/store.js
@@ -1,4 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/reducers';
 
-export default createStore(rootReducer);
+const loggerMiddleware = createLogger();
+export default function configureStore(preloadedState = null) {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+}
+
+// export default createStore(rootReducer);
--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import { Provider } from 'react-redux';
 import 'normalize.css';
 import 'styles/main.css';
 
-import store from './app-redux/store/store';
+import configureStore from './app-redux/store/store';
 
 import AppWrapper from './App/AppWrapper';
 import registerServiceWorker from './registerServiceWorker';
+
+const preloadedState = {
+  selectedUser: null,
+  usersList: [],
+};
+
+const store = configureStore(preloadedState);
 
 ReactDOM.render(
   <Provider store={store}>
